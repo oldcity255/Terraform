ed0568160c068d758438cdd12f42d7b4df0fdacc
Vepa Gaiypov <oldcity255@gmail.com>
2021-03-15 02:24:57 -0700

    new updates

--- key_pair.tf
+++ key_pair.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "my_key" {
-  key_name   = "my_key"
+  key_name   = var.key_name
   public_key = file("~/.ssh/id_rsa.pub")
 
 
--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,8 @@
 resource "aws_vpc" "Vepa_Vpc" {
-  cidr_block       = "10.2.0.0/16"
-  instance_tenancy = "default"
+  cidr_block       = var.cidr_block
+  instance_tenancy = var.instance_tenancy
+
+  
   tags = {
     Name = "Humay_Vpc"
     Department = "Production"
